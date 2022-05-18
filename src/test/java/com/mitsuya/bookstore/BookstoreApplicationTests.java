314d37773fedffc3b742695181ffa56e23002713
HKSMitsuya <rodrigosouza0625@gmail.com>
2022-05-18 17:14:49 -0300

    commit inicial

---
+++ src/test/java/com/mitsuya/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitsuya.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
